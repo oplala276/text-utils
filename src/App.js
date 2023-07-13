9a6530f73b04b47b4097aba511632a9726841bd0
Oplala <oplala2020@gmail.com>
2023-07-13 23:54:29 +0530

    changes accepted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+// import About from './components/About';
 function App() {
   return (
     <>
       <Navbar />
       <div className="container my-4">
-
+        {/* <About /> */}
         <TextForm />
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,14 +6,21 @@ export default function TextForm() {
         let newText = text.toUpperCase();
         // console.log("cahnged" + text)
         setText(newText)
-
+    }
+    const changetextlo = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const cleartext = () => {
+        let newText = "";
+        setText(newText)
     }
     const handlechange = (e) => {
         setText(e.target.value);
     }
     const [text, setText] = useState("");
     return (
-        <div>
+        <div id='josa' >
             <h1>Enter Text to Analyze</h1>
             {/* <label for="exampleColorInput" class="form-label">Color picker</label>
             <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color"></input> */}
@@ -21,7 +28,15 @@ export default function TextForm() {
                 {/* <label htmlFor="exampleFormControlTextarea1" className="form-label"></label> */}
                 <textarea className="form-control" id="myBox" placeholder='enter text' value={text} onChange={handlechange} rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={changetext}>Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={changetext}>Uppercase</button>
+            <button className="btn btn-success mx-2" onClick={changetextlo}>Lowercase</button>
+            <button className="btn btn-danger" onClick={cleartext}>Clear</button>
+            <h2>Text Summary</h2>
+            <p>{text.split(" ").length} words {text.length} characters</p>
+            <p>{text.split(" ").length * 0.008} minutes to read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+
         </div>
     )
 }
