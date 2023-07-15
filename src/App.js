82ae6bd577ff8ac441395a36ebc6c2a38592421b
Oplala <oplala2020@gmail.com>
2023-07-16 00:11:41 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 // import About from './components/About';
 function App() {
+  const [mode, setMode] = useState('light');
+  const [text, setText] = useState({
+    color: 'black',
+  });
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggle = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'rgb(0 101 122)';
+      showAlert("Dark mode is enabled", "success")
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is enabled", "primary")
+    }
+  }
+
   return (
     <>
-      <Navbar />
+      <Navbar mode={mode} toggle={toggle} />
+      <Alert alert={alert} />
       <div className="container my-4">
-        {/* <About /> */}
-        <TextForm />
+        <TextForm mode={mode} />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,37 @@
 // import { useState } from "react"
 import React, { useState } from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
+    const [mystyle, setMystyle] = useState({
+        color: 'black',
+        background: 'white'
+    }
+    )
+    const [btntext, setBtntext] = useState({
+        background: 'orange',
+        color: 'black',
+    })
+    const togol = () => {
+        if (mystyle.background === 'white') {
+            setMystyle({
+                color: 'white',
+                background: 'black'
+            })
+            setBtntext({
+                background: 'blue',
+                color: 'black',
+            })
+        } else {
+            setMystyle({
+                color: 'black',
+                background: 'white'
+            })
+            setBtntext({
+                background: 'blue',
+                color: 'black',
+            })
+        }
+    }
     const changetext = () => {
         let newText = text.toUpperCase();
         // console.log("cahnged" + text)
@@ -20,13 +50,13 @@ export default function TextForm() {
     }
     const [text, setText] = useState("");
     return (
-        <div id='josa' >
+        <div id='josa' style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
             <h1>Enter Text to Analyze</h1>
             {/* <label for="exampleColorInput" class="form-label">Color picker</label>
             <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color"></input> */}
             <div className="mb-3">
                 {/* <label htmlFor="exampleFormControlTextarea1" className="form-label"></label> */}
-                <textarea className="form-control" id="myBox" placeholder='enter text' value={text} onChange={handlechange} rows="8"></textarea>
+                <textarea className="form-control" id="myBox" placeholder='enter text' value={text} onChange={handlechange} style={{ background: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? 'black' : 'white' }} rows="8" ></textarea>
             </div>
             <button className="btn btn-primary" onClick={changetext}>Uppercase</button>
             <button className="btn btn-success mx-2" onClick={changetextlo}>Lowercase</button>
@@ -35,7 +65,7 @@ export default function TextForm() {
             <p>{text.split(" ").length} words {text.length} characters</p>
             <p>{text.split(" ").length * 0.008} minutes to read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text : "Enter your text to preview"}</p>
 
         </div>
     )
